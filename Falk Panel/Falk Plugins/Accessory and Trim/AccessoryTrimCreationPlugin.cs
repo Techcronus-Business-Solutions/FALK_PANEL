a411bfcf4b6608e06c87fc2707a2f121393b16bf
Ritika Chavda <ritika.chavda@techcronus.com>
2026-07-06 15:56:09 +0530

    Accessory & Trim Creation On Opportunity Product Creation

---
+++ Falk Panel/Falk Plugins/Accessory and Trim/AccessoryTrimCreationPlugin.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Falk_Plugins.Pricing_Master
+{
+    public class AccessoryTrimCreationPlugin : PluginBase
+    {
+        public AccessoryTrimCreationPlugin() : base(typeof(AccessoryTrimCreationPlugin)) { }
+        #region Private Variables
+        private IOrganizationService service { get; set; }
+        private IPluginExecutionContext context { get; set; }
+        private ITracingService tracingService { get; set; }
+        private IOrganizationServiceFactory factory { get; set; }
+        private Entity targetEntity { get; set; }
+        #endregion
+        protected override void ExecuteCrmPlugin(LocalPluginContext localcontext)
+        {
+            if (localcontext == null)
+                throw new ArgumentNullException(nameof(localcontext)); 
+             
+            InitProperties(localcontext);
+
+            try
+            {
+                if (context.MessageName != "Create")
+                    return;
+
+                tracingService.Trace("Plugin Started");
+
+                if (context.Depth > 1)
+                    return;
+
+                if (!context.InputParameters.Contains("Target"))
+                    return;
+
+                if (!(context.InputParameters["Target"] is Entity target))
+                    return;
+
+                if (target.LogicalName != "opportunityproduct")
+                    return;
+
+                tracingService.Trace("Opportunity Product Id");
+
+                Entity opportunityProduct = service.Retrieve(
+                                "opportunityproduct",
+                                target.Id,
+                                new ColumnSet(
+                                    "opportunityproductname",
+                                    "productid",
+                                    "tbs_panelthickness"));
+
+                string opportunityProductName = opportunityProduct.GetAttributeValue<string>("opportunityproductname");
+
+                EntityReference panelThickness = opportunityProduct.GetAttributeValue<EntityReference>("tbs_panelthickness");
+
+                tracingService.Trace("Panel Thickness Id : " + panelThickness.Id.ToString());
+
+                if (panelThickness == null)
+                    return;
+
+                EntityReference panelType = opportunityProduct.GetAttributeValue<EntityReference>("productid");
+
+                CreatePanelAccessories(service, target.Id, opportunityProductName, panelType, panelThickness);
+
+                CreatePanelTrims(service, target.Id, opportunityProductName, panelType, panelThickness);
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace($"CreatePanelAccessoryAndTrim Error : {ex}");
+                throw new InvalidPluginExecutionException("Error occurred while creating Panel Accessory and Panel Trim records.", ex);
+            }
+        }
+        private void CreatePanelAccessories(IOrganizationService service, Guid opportunityProductId, string opportunityProductName, EntityReference panelType, EntityReference panelThickness)
+        {
+            try
+            {
+                tracingService.Trace("===== ACCESSORY FETCH =====");
+
+                string fetchXml = $@"<fetch distinct='true'>
+                  <entity name='tbs_accessory'>
+                    <attribute name='tbs_accessoryid' />
+                    <attribute name='tbs_name' />
+                    <link-entity
+                        name='tbs_accessory_tbs_thickness'
+                        from='tbs_accessoryid'
+                        to='tbs_accessoryid'
+                        intersect='true'>
+                      <filter>
+                        <condition
+                          attribute='tbs_thicknessid'
+                          operator='eq'
+                          value='{panelThickness.Id}' />
+                      </filter>
+                    </link-entity>
+                  </entity>
+                </fetch>";
+
+                tracingService.Trace(fetchXml);
+
+                EntityCollection accessories = service.RetrieveMultiple(new FetchExpression(fetchXml));
+               
+                foreach (Entity accessory in accessories.Entities)
+                {
+                    Entity panelAccessory = new Entity("tbs_opppanelaccessory");
+
+                    panelAccessory["tbs_name"] = $"{opportunityProductName} | {accessory.GetAttributeValue<string>("tbs_name")}";
+
+                    panelAccessory["tbs_opportunityproduct"] = new EntityReference("opportunityproduct", opportunityProductId);
+
+                    panelAccessory["tbs_paneltype"] = panelType;
+
+                    panelAccessory["tbs_panelthickness"] = panelThickness;
+
+                    panelAccessory["tbs_accessory"] = accessory.ToEntityReference();
+
+                    service.Create(panelAccessory);
+                }
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace($"CreatePanelAccessoryAndTrim Error : {ex}");
+                throw new InvalidPluginExecutionException("Error occurred while creating Panel Accessory and Panel Trim records.", ex);
+            }
+        }
+        private void CreatePanelTrims(IOrganizationService service, Guid opportunityProductId, string opportunityProductName, EntityReference panelType, EntityReference panelThickness)
+        {
+            try
+            {
+                string fetchXml = $@"
+                <fetch distinct='true'>
+                  <entity name='tbs_trim'>
+                    <attribute name='tbs_trimid' />
+                    <attribute name='tbs_name' />
+                    <link-entity
+                        name='tbs_trim_tbs_thickness'
+                        from='tbs_trimid'
+                        to='tbs_trimid'
+                        intersect='true'>
+                      <filter>
+                        <condition
+                          attribute='tbs_thicknessid'
+                          operator='eq'
+                          value='{panelThickness.Id}' />
+                      </filter>
+                    </link-entity>
+                  </entity>
+                </fetch>";
+
+                tracingService.Trace("Trim");
+
+                EntityCollection trims = service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+                foreach (Entity trim in trims.Entities)
+                {
+                    Entity panelTrim = new Entity("tbs_opppaneltrim");
+
+                    panelTrim["tbs_name"] = $"{opportunityProductName} | {trim.GetAttributeValue<string>("tbs_name")}";
+
+                    panelTrim["tbs_opportunityproduct"] = new EntityReference("opportunityproduct", opportunityProductId);
+
+                    panelTrim["tbs_paneltype"] = panelType;
+
+                    panelTrim["tbs_panelthickness"] = panelThickness;
+
+                    panelTrim["tbs_trim"] = trim.ToEntityReference();
+
+                    service.Create(panelTrim);
+                }
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace($"CreatePanelAccessoryAndTrim Error : {ex}");
+                throw new InvalidPluginExecutionException("Error occurred while creating Panel Accessory and Panel Trim records.", ex);
+            }
+        }
+        private void InitProperties(LocalPluginContext localcontext)
+        {
+            //// Obtain the execution context service from the LocalContext.
+            context = localcontext.PluginExecutionContext;
+            if (context == null)
+            {
+                throw new InvalidPluginExecutionException("Failed to retrieve Plugin Execution Context !");
+            }
+
+            //Get the Organization Service from the LocalContext
+            service = localcontext.OrganizationService;
+            if (service == null)
+            {
+                throw new InvalidPluginExecutionException("Failed to retrieve Organization Service !");
+            }
+
+            //Get the Tracing Service from the LocalContext
+            tracingService = localcontext.TracingService;
+            if (tracingService == null)
+            {
+                throw new InvalidPluginExecutionException("Failed to retrieve Tracing Service !");
+            }
+        }
+    }
+}
