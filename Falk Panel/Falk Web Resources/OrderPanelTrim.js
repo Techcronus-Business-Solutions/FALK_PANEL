0146259485e772cb993535108f56c9ed1521c2ca
Niki Patel <niki.patel@promx.net>
2026-08-19 10:19:45 +0530

    Hide show section js added for order

---
+++ Falk Panel/Falk Web Resources/OrderPanelTrim.js
@@ -0,0 +1,33 @@
+﻿if (typeof Falk === "undefined") {
+    Falk = {
+        __namespace: true,
+    };
+}
+if (typeof $ === "undefined") {
+    $ = parent.$;
+    Jquery = parent.Jquery;
+}
+
+Falk.OrderPanelTrim = {
+    OnLoad: function (executionContext) {
+        const formContext = executionContext.getFormContext();
+
+        Falk.OrderPanelTrim.ToggleCustomTrimSection(executionContext);
+
+        formContext.getAttribute("tbs_iscustomtrim").addOnChange(Falk.OrderPanelTrim.ToggleCustomTrimSection);
+    },
+
+    ToggleCustomTrimSection: function (executionContext) {
+        const formContext = executionContext.getFormContext();
+
+        const isCustomTrim = formContext.getAttribute("tbs_iscustomtrim").getValue();
+
+        const section = formContext.ui.tabs
+            .get("General_Tab")
+            .sections.get("CustomTrim_Section");
+
+        if (section) {
+            section.setVisible(isCustomTrim === true);
+        }
+    }
+}
